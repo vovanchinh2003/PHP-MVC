6de715e333c9bfd6ba97cad0c136b3191f72eac7
vovanchinh2003 <129940188+vovanchinh2003@users.noreply.github.com>
2023-06-19 13:42:44 +0700

    Update baitapmang.php

--- baitapmang.php
+++ baitapmang.php
@@ -4,7 +4,7 @@
     echo "a[$key]=$value <br/>";
 
     echo "-------------<br/>";
-    $b = array("1555","23","10","5");
+    $b = array("1555","288883","10","5");
     $sum =0;
     foreach($b as $key=>$value){
     $sum += $value;
